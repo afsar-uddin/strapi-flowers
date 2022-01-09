7a4814bf50e35fc8cc5303eb83d45b0f1fa14468
Afsar Uddin <afsarwebdev@gmail.com>
2022-01-09 09:05:13 +0600

    graphql post limit issue fixed

--- frontend/src/pages/BlogPosts.js
+++ frontend/src/pages/BlogPosts.js
@@ -3,8 +3,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const BLOGS = gql`
-    query GetBlogs{
-        blogs {
+    query{
+        blogs(pagination: { page: 1, pageSize: 100 }) {
             data {
                 id,
                 attributes {
@@ -28,6 +28,14 @@ const BLOGS = gql`
                     }
                 }
             }
+            meta {
+                pagination {
+                  page
+                  pageSize
+                  pageCount
+                  total
+                }
+              }
         }
     }
 `
@@ -35,11 +43,12 @@ const BLOGS = gql`
 export default function BlogPosts() {
     const { loading, error, data } = useQuery(BLOGS)
 
-    // console.log(data)
+    // console.log(data.blogs.data.length)
     if (loading) return <p>Loading...</p>
     if (error) return <p>There is something wrong, check your data again...  </p>
     return (
         <div className='blog-posts'>
+            <h1>Total {data.blogs.data.length} items</h1>
             {
                 data.blogs.data.map(blog => (
                     // console.log(blog.attributes.title)
@@ -57,7 +66,7 @@ export default function BlogPosts() {
                                     }
                                 </div>
                                 <img src={`http://localhost:1337${blog.attributes.fig.data.attributes.url}`} />
-                                <p>{blog.attributes.desc.substring(0, 300)}...</p>
+                                <p>{blog.attributes.desc.substring(0, 40)}...</p>
                                 <Link to={`/blog-detail/${blog.id}`}>View more</Link>
                             </div>
                         </div>
